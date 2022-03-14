1d281018096b8dfba1d1a72bf74516e501318a37
amirkarimistr <amirkarimistr@gmail.com>
2022-03-14 13:25:50 +0330

    feat -> Add a fake access token as header

--- lib/src/flowder.dart
+++ lib/src/flowder.dart
@@ -47,9 +47,11 @@ class Flowder {
       final file = await options.file.create(recursive: true);
       final response = await client.get(
         url,
-        options: Options(
-            responseType: ResponseType.stream,
-            headers: {HttpHeaders.rangeHeader: 'bytes=$lastProgress-'}),
+        options: Options(responseType: ResponseType.stream, headers: {
+          HttpHeaders.rangeHeader: 'bytes=$lastProgress-',
+          "Bearer":
+              "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5MzcxNTciLCJqdGkiOiIzNjg3M2Y2NDMwYjI3OGRhMmRkOWYzMzkyMGNiMTRhMmJlODU4YjVlOTIwNGZlY2I4Yjk0Njk5ZGU2YjQxZjE2NjQ3NDUxNzgxY2I5ZjExZSIsImlhdCI6MTY0NzI1MDk1OSwibmJmIjoxNjQ3MjUwOTU5LCJleHAiOjE2NDk5MjU3NTksInN1YiI6IlVzZXIuMmZkODQ5NzAtMDU3Ni0xMWVjLTlmYTAtY2RkODY4MGNmZjM2Iiwic2NvcGVzIjpbXX0.ika4orAWHGvhQYpbnuBqjgQYM37fN5rMBo92KOhJRTM1z8WYRbdUVBLBAxaBAacrAkFk0cgA68clPuYEDR3andof8VMEuhyDiE-lKSZhY_9V2C0aVNyTvtlquh8L_hVSZLVpEXmuqI9j5zqmgrLVzpIDaV5HMNaV6AQSgDkFRhPcy2lzzABSIfJ2cAjy8AgP0KVW_UK2-weQ8MJ9iduyuw28kxlMuyyUAYojDMftEtmvm44KKexT7PkaQtl_i5T7RylV-GE6X8yBe8YZgBygzkaQ8u41zLdynJY1MAfATq_CMEh_0AGKAGjwcBGzsO1_HqqgggrN0zPydsISFGICgrqJdPVrPmGxKBqeg5nDLP_oUBVQlXAqd26Gn96L3lQvY4c64SVpu4aVepSB2lK0FY0Fyn7k1mVjYGIP8B50cHfsN8TqKM5_7Yvisq1gjW3305kTq-WlCm47327wdN6BNttMTnfgz_7isldC7UTC5JNQFWAQ0aUAT_oFW23Ishxv3zm3uvGBSF2LO4QvcN3yiQZ--QsZkgxO7oPKh4XCyDnW5srvzo0DJgwWWO7gy2iRhP3Ib33yt8t-d2Gk-m2ktN_Xd1lHg2DkMISaqZJ6jEMkC1zPWDYQE4qrsLmdHKop5lNyWecZo83wT9WaXkv1ZBo1LDUp149rVGjucXj0jEg"
+        }),
       );
       final _total = int.tryParse(
               response.headers.value(HttpHeaders.contentLengthHeader)!) ??
