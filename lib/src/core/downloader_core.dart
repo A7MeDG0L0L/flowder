d42a94f6febe50e1b5a93c15065f6d00fd49519a
amirkarimistr <amirkarimistr@gmail.com>
2022-04-03 16:47:12 +0430

    new changes

--- lib/src/core/downloader_core.dart
+++ lib/src/core/downloader_core.dart
@@ -9,7 +9,7 @@ import 'package:flowder/src/utils/downloader_utils.dart';
 /// also required to actually `start`,`stop`,`pause`,`cancel` a download.
 class DownloaderCore {
   /// StreamSubscription used to link with the download streaming.
-  late CancelToken? _cancelToken;
+  late CancelToken _inner;
 
   /// Inner utils
   late final DownloaderUtils _options;
@@ -20,24 +20,29 @@ class DownloaderCore {
   /// Check if the download was cancelled.
   bool isCancelled = false;
 
-  DownloaderCore(CancelToken? cancelToken, DownloaderUtils options, String url)
-      : _cancelToken = cancelToken,
+  DownloaderCore(CancelToken cancelToken, DownloaderUtils options, String url)
+      : _inner = cancelToken,
         _options = options,
         _url = url;
 
   /// Pause any current download.
   Future<void> pause() async {
     _isActive();
-    _cancelToken?.cancel();
+    _inner.cancel();
     isDownloading = false;
   }
 
-
+  /// Resume any current download, with the pending progress.
+  Future<void> resume() async {
+    _isActive();
+    if (isDownloading) return;
+    _inner = await Flowder.initDownload(_url, _options);
+  }
 
   /// Cancel any current download, even if the download is [pause]
   Future<void> cancel() async {
     _isActive();
-    _cancelToken!.cancel();
+    _inner.cancel();
     await _options.progress.resetProgress(_url);
     if (_options.deleteOnCancel) {
       await _options.file.delete();
@@ -56,8 +61,8 @@ class DownloaderCore {
   Future<DownloaderCore> download(String url, DownloaderUtils options) async {
     try {
       // ignore: cancel_subscriptions
-      await Flowder.initDownload(url, options);
-      return DownloaderCore(_cancelToken, options, url);
+      final subscription = await Flowder.initDownload(url, options);
+      return DownloaderCore(subscription, options, url);
     } catch (e) {
       rethrow;
     }
